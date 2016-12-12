756f66510108e25f29305732a29f9b78b65191bb
Jenia <genia90@gmail.com>
2016-12-12 19:55:26 +0200

    Initial commit

---
+++ Parser.py
@@ -0,0 +1,13 @@
+class Parser:
+    def __init__(self, file):
+        self.file = file
+        self.cmdTypeList = ['A_COMMAND', 'C_COMMAND', 'L_COMMAND']
+
+    def hasMoreCommand(self):
+        return
+
+    def advance(self):
+        return
+
+    def commandType(self):
+        return
