3c3e343a326e2138d9ad3e7246b1173fb3097660
sharon1832 <liad.elidan@mail.huji.ac.il>
2016-12-15 14:47:15 +0200

    shitasd

--- ex6/Parser.py
+++ ex6/Parser.py
@@ -9,13 +9,20 @@ COMMENT = '//'
 A_COMMAND = 'A_COMMAND'
 C_COMMAND = 'C_COMMAND'
 L_COMMAND = 'L_COMMAND'
-
+EMPTY_STRING = 0
+CORRECT_SYNTAX = 1
+COMMAND_START = 1
+COMMAND_ENDS = -1
+DEST = 0
+COMP = 0
+SPLITTING = 1
+JUMP = 1
 
 class Parser:
     def __init__(self, file):
         self.fileLines = [l.strip() for l in file.readlines()
                           if not l.strip().startswith(COMMENT)
-                          and len(l.strip()) > 0]
+                          and len(l.strip()) > EMPTY_STRING]
         self.currentIndex = 0
 
     def hasMoreCommands(self):
@@ -26,7 +33,7 @@ class Parser:
 
     def commandType(self):
         command = self.fileLines[self.currentIndex]
-        if command.startswith(AT) and len(command) > 1:
+        if command.startswith(AT) and len(command) > CORRECT_SYNTAX:
             return A_COMMAND
         elif command.startswith(LBRKT) and command.endswith(RBRKT):
             return L_COMMAND
@@ -35,18 +42,18 @@ class Parser:
 
     def symbol(self):
         if self.commandType() is A_COMMAND:
-            return self.fileLines[self.currentIndex][1:]
+            return self.fileLines[self.currentIndex][COMMAND_START:]
         elif self.commandType() is L_COMMAND:
-            return self.fileLines[self.currentIndex][1:-1]
+            return self.fileLines[self.currentIndex][COMMAND_START:COMMAND_ENDS]
 
     def dest(self):
         if self.commandType() is C_COMMAND:
-            return Code.dest(self.fileLines[self.currentIndex].split(EQU)[0])
+            return Code.dest(self.fileLines[self.currentIndex].split(EQU)[DEST])
 
     def comp(self):
         if self.commandType() is C_COMMAND:
-            return Code.comp(self.fileLines[self.currentIndex].split(EQU)[1].split(SEMIC)[0])
+            return Code.comp(self.fileLines[self.currentIndex].split(EQU)[SPLITTING].split(SEMIC)[COMP])
 
     def jump(self):
         if self.commandType() is C_COMMAND:
-            return Code.jump(self.fileLines[self.currentIndex].split(SEMIC)[1])
+            return Code.jump(self.fileLines[self.currentIndex].split(SEMIC)[JUMP])
