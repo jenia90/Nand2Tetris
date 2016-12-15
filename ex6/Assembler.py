1fe223782ce0556dc37d52bac89a28f4677d3b85
Jenia <genia90@gmail.com>
2016-12-15 14:42:48 +0200

    Some cleanups and stuff

--- ex6/Assembler.py
+++ ex6/Assembler.py
@@ -2,15 +2,16 @@ import sys
 
 from Code import *
 from SymbolTable import *
-
 from Parser import *
 
 
 def main():
     inp = sys.argv[1]
     file = open(inp, 'r')
-    #filename = file.name.split('.', '\\')
+    filename = file.name
     parser = Parser(file)
+
+    file.close()
 
 
 
--- ex6/Parser.py
+++ ex6/Parser.py
@@ -1,13 +1,20 @@
-import Code
+from Code import *
+
+AT = '@'
+EQU = '='
+SEMIC = ';'
+LBRKT = '('
+RBRKT = ')'
+COMMENT = '//'
+A_COMMAND = 'A_COMMAND'
+C_COMMAND = 'C_COMMAND'
+L_COMMAND = 'L_COMMAND'
+
+
 class Parser:
-
-    A_COMMAND = 'A_COMMAND'
-    C_COMMAND = 'C_COMMAND'
-    L_COMMAND = 'L_COMMAND'
-
     def __init__(self, file):
         self.fileLines = [l.strip() for l in file.readlines()
-                          if not l.strip().startswith('//')
+                          if not l.strip().startswith(COMMENT)
                           and len(l.strip()) > 0]
         self.currentIndex = 0
 
@@ -19,27 +26,27 @@ class Parser:
 
     def commandType(self):
         command = self.fileLines[self.currentIndex]
-        if command.startswith('@') and len(command) > 1:
-            return self.A_COMMAND
-        elif command.startswith('(') and command.endswith(')'):
-            return self.L_COMMAND
-        elif command.contains('=',';'):
-            return self.C_COMMAND
+        if command.startswith(AT) and len(command) > 1:
+            return A_COMMAND
+        elif command.startswith(LBRKT) and command.endswith(RBRKT):
+            return L_COMMAND
+        elif EQU in command and SEMIC in command:
+            return C_COMMAND
 
     def symbol(self):
-        if self.commandType() is self.A_COMMAND:
+        if self.commandType() is A_COMMAND:
             return self.fileLines[self.currentIndex][1:]
-        elif self.commandType() is self.L_COMMAND:
+        elif self.commandType() is L_COMMAND:
             return self.fileLines[self.currentIndex][1:-1]
 
     def dest(self):
-        if self.commandType() is self.C_COMMAND:
-            return Code.Code.dest(self.fileLines[self.currentIndex].split('=')[0])
+        if self.commandType() is C_COMMAND:
+            return Code.dest(self.fileLines[self.currentIndex].split(EQU)[0])
 
     def comp(self):
-        if self.commandType() is self.C_COMMAND:
-            return Code.Code.comp(self.fileLines[self.currentIndex].split('=')[1].split(';')[0])
+        if self.commandType() is C_COMMAND:
+            return Code.comp(self.fileLines[self.currentIndex].split(EQU)[1].split(SEMIC)[0])
 
     def jump(self):
-        if self.commandType() is self.C_COMMAND:
-            return Code.Code.jump(self.fileLines[self.currentIndex].split(';')[1])
+        if self.commandType() is C_COMMAND:
+            return Code.jump(self.fileLines[self.currentIndex].split(SEMIC)[1])
