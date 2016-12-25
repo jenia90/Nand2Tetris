ab3763bdeccd80caf8c411eba7d1d45f1cfa6e1e
sharon1832 <liad.elidan@mail.huji.ac.il>
2016-12-25 16:47:24 +0200

    parser liads

--- ex7/Parser.py
+++ ex7/Parser.py
@@ -17,11 +17,12 @@ class Parser:
     """
 
     def __init__(self, file):
-        self._lines = [l.split(COMMENT)[0].strip() for l in file.readlines()
+        self.fileLines = [l.split(COMMENT)[0].strip() for l in file.readlines()
                        if not l.strip().startswith(COMMENT)
                        and len(l.strip()) > 0]
+        self._currCommandArray = []
         self._currentIndex = 0
-        self._currentCommand = ''
+        self._currentLine = ''
         self._currCommType = ''
         self._updateCurrentCommand()
 
@@ -37,7 +38,7 @@ class Parser:
         Advances the current command pointer to the next command
         """
         self.currentIndex += 1
-        self.updateCurrentCommand()
+        self._updateCurrentCommand()
 
     def _updateCurrentCommand(self):
         """
@@ -46,8 +47,8 @@ class Parser:
         """
         if not self.hasMoreCommands():
             return
-        self.currentCommand = \
-            self.fileLines[self.currentIndex % len(self.fileLines)]
+        self._currentLine = self.fileLines[self.currentIndex % len(self.fileLines)]
+        self._currCommandArray = self._currentLine.split()
         self.currCommType = self.commandType()
 
     def getCurrentType(self):
@@ -61,25 +62,45 @@ class Parser:
         Resets the current command pointer
         """
         self.currentIndex = 0
-        self.updateCurrentCommand()
+        self._updateCurrentCommand()
 
     def commandType(self):
         """
         Gets the type of the current command
         :return: Command type string
         """
-        return
+        cArithmeticCommands = {'add','sub','neg','eq','gt','lt','and','or','not'}
+        if self._currCommandArray[0] in cArithmeticCommands:
+            return ARITHMETIC_COMM
+        elif self._currCommandArray[0] == 'pop':
+            return POP_COMM
+        elif self._currCommandArray[0] == 'push':
+            return PUSH_COMM
+        elif self._currCommandArray[0] == 'label':
+            return LABEL_COMM
+        elif self._currCommandArray[0] == 'goto':
+            return GOTO_COMM
+        elif self._currCommandArray[0] == 'if-goto':
+            return IF_COMM
+        elif self._currCommandArray[0] == 'function':
+            return FUNCTION_COMM
+        elif self._currCommandArray[0] == 'call':
+            return FUNCTION_COMM
+        elif self._currCommandArray[0] == 'return':
+            return FUNCTION_COMM
 
     def arg1(self):
         """
         First command argument
         :return: first command argument string
         """
-        return
+        if len(self._currCommandArray)>1:
+            return self._currCommandArray[1]
 
     def arg2(self):
         """
         Second command argument
         :return: second command argument string
         """
-        return
+        if len(self._currCommandArray)>2:
+            return self._currCommandArray[2]
