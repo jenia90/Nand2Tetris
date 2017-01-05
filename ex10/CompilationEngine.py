7186ee2f82a2cd5f5ce1e546ad1be9cdfca7818f
sharon1832 <liad.elidan@mail.huji.ac.il>
2017-01-05 15:44:34 +0200

    gfhadsfajkfhj

--- ex10/CompilationEngine.py
+++ ex10/CompilationEngine.py
@@ -1,9 +1,29 @@
+import xml.etree.cElementTree as ET
+import JackTockenizer
+
 class CompilationEngine:
-    def __init__(self):
-        return
+    def __init__(self, tokenizer):
+        self._tokenizer = tokenizer
+        self._root = ET.Element("class")
+        self._tree = ET.ElementTree(self._root)
+        self._output = ''
 
     def CompileClass(self):
-        return
+        while self._tokenizer.hasMoreCommands():
+            currToken = self._tokenizer.tokenType()
+            if currToken == KEYWORD:
+                ET.SubElement(self._root,KEYWORD).text = self._tokenizer.keyWord()
+            elif currToken == SYMBOL:
+                ET.SubElement(self._root,SYMBOL).text = self._tokenizer.symbol()
+            elif currToken == IDENTIFIER:
+                ET.SubElement(self._root,IDENTIFIER).text = self._tokenizer.identifier()
+            elif currToken == INT_CONST:
+                ET.SubElement(self._root,INT_CONST).text = self._tokenizer.intVal()
+            elif currToken == STRING_CONST:
+                ET.SubElement(self._root,STRING_CONST).text = self._tokenizer.stringVal()
+            else:
+                return "ERROR"
+        self._tokenizer.advance()
 
     def CompileClassVarDec(self):
         return
