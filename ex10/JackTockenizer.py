90e5052b517d0b7a2b31bebbfa824560df13cc7f
Jenia <genia90@gmail.com>
2017-01-05 13:38:00 +0200

    some regexes done

--- ex10/JackTockenizer.py
+++ ex10/JackTockenizer.py
@@ -1,5 +1,42 @@
+import re
+
+GRP_TYPE_IDX = 1
+
+IF_EXPR_IDX = 2
+IF_STMNT_IDX = 3
+ELIF_STMNT_IDX = 4
+
+VAR_NAME_IDX = 2
+ARR_LEN_IDX = 3
+ASS_IDX = 4
+
+DO_ROUTINE_IDX = 2
+
+RET_IDX = 2
+
+IF_EXP = '(if|while)\h+\(\h*([^)]+)\h*\)\s*\{\s*([^}]+)\s*\}\s*' \
+         '(?:(else)\s*\{([^)]+)\})?'
+LET_EXP = '^(let)\h+(\D[^\[ ]*)(?:\[(\d+)\])?\h*=\h*([^;]+);$'
+DO_EXP = '^(do)\h+(\D[^;]*);$'
+RET_EXP = '^(return)(?:\h*([^;]+))?;$'
+
+KWD_LIST = ['class', 'constructor', 'function', 'method', 'field', 'static',
+            'var', 'int', 'char', 'boolean', 'void', 'true', 'false',
+            'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
+
+SYMBOL_LIST = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*',
+               '/', '&', '|', '<', '>', '=', '~']
+
+INT_MIN = 0
+INT_MAX = 32767
+
+
 class JackTockenizer:
     def __init__(self):
+        self.re_cond = re.compile(IF_EXP)
+        self.re_let = re.compile(LET_EXP)
+        self.re_do = re.compile(DO_EXP)
+        self.re_ret = re.compile(RET_EXP)
         return
 
     def hasMoreTokens(self):
