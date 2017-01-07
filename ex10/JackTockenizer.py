5be2ea5a6e618a71dbb78a7c2dfdf84e70359c79
Jenia <genia90@gmail.com>
2017-01-07 23:11:28 +0200

    gotta figure out the splitting to tokens!!!!!!!!!!!!

--- ex10/JackTockenizer.py
+++ ex10/JackTockenizer.py
@@ -1,31 +1,21 @@
 import re
 
-COMMENT = '//'
+SINGLE_COMMENT = '//'
 KEYWORD = 'keyword'
 SYMBOL = 'symbol'
 IDENTIFIER = 'identifier'
 INT_CONST = 'intConstant'
 STRING_CONST = 'stringConstant'
 
-GRP_TYPE_IDX = 1
+CLASS = 'class'
+VARDEC = 'varDec'
+SUBROUTINE = 'subRoutine'
 
-IF_EXPR_IDX = 2
-IF_STMNT_IDX = 3
-ELIF_STMNT_IDX = 4
+classVarList = ['static', 'field']
+varTypeList = ['int', 'char', 'boolean']
+subroutineTypeList = ['constructor', 'function', 'method']
+STATEMENTS = ['if', 'else', 'while', 'return', 'do', 'let']
 
-VAR_NAME_IDX = 2
-ARR_LEN_IDX = 3
-ASS_IDX = 4
-
-DO_ROUTINE_IDX = 2
-
-RET_IDX = 2
-
-IF_EXP = '(if|while)\h+\(\h*([^)]+)\h*\)\s*\{\s*([^}]+)\s*\}\s*' \
-         '(?:(else)\s*\{([^)]+)\})?'
-LET_EXP = '^(let)\h+(\D[^\[ ]*)(?:\[(\d+)\])?\h*=\h*([^;]+);$'
-DO_EXP = '^(do)\h+(\D[^;]*);$'
-RET_EXP = '^(return)(?:\h*([^;]+))?;$'
 
 KWD_LIST = ['class', 'constructor', 'function', 'method', 'field', 'static',
             'var', 'int', 'char', 'boolean', 'void', 'true', 'false',
@@ -34,7 +24,7 @@ KWD_LIST = ['class', 'constructor', 'function', 'method', 'field', 'static',
 SYMBOL_LIST = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*',
                '/', '&', '|', '<', '>', '=', '~']
 
-
+symbolConvertion = {'<': '&lt;', '>': '&gt;', '"': '&quot;', '&': '&amp;'}
 
 INT_MIN = 0
 INT_MAX = 32767
@@ -42,38 +32,94 @@ INT_MAX = 32767
 
 class JackTockenizer:
     def __init__(self, file):
-        self.tokens = [l.split(COMMENT).strip().split() for l in
-                       file.readlines()
-                       if not l.strip().startswith(COMMENT)
-                       and len(l.strip()) > 0]
-        self.re_cond = re.compile(IF_EXP)
-        self.re_let = re.compile(LET_EXP)
-        self.re_do = re.compile(DO_EXP)
-        self.re_ret = re.compile(RET_EXP)
-
+        self._tokens = [l.split(SINGLE_COMMENT)[0].strip() for l in
+                        file.readlines()
+                        if not l.startswith(SINGLE_COMMENT)
+                        and len(l) > 0]
+        self.removeComments()
+        self.splitSymbols()
         self.currentIndex = 0
-        return
+        self._currentToken = self._tokens[0]
+
+    def removeComments(self):
+        newTokens = []
+        for i in self._tokens:
+            if i.startswith('/**'):
+                while not i.endswith('*/'):
+                    continue
+            elif i.startswith(SINGLE_COMMENT):
+                continue
+            else:
+                newTokens.append(i)
+
+        self._tokens = newTokens
+
+    def splitSymbols(self):
+        newTokens = []
+        for i in self._tokens:
+            for l in i:
+                if l in SYMBOL_LIST:
+                    newTokens.append(l)
+            newTokens.append(i)
 
     def hasMoreTokens(self):
-        return self.currentIndex < len(self.tokens)
+        """
+        Checks if there are more tokens in the input.
+        :return: true if there are more tokens; false otherwise.
+        """
+        return self.currentIndex < len(self._tokens)
 
     def advance(self):
+        """
+        Get the nest token from the input and makes it the current token.
+        """
+        while self.hasMoreTokens()
+            if
         self.currentIndex += 1
 
+    def validate(self, str):
+        if self.re_cond.match(str):
+            return
+
     def tokenType(self):
+        """
+        Returns the type of the current token
+        :return: Token type constant.
+        """
         return
 
     def keyWord(self):
+        """
+        Returns the keyword which is the current token.
+        :return:
+        """
         return
 
     def symbol(self):
+        """
+        Returns the character which is the current token.
+        :return:
+        """
         return
 
     def identifier(self):
+        """
+        Returns the identifier which is the current token.
+        :return:
+        """
         return
 
     def intVal(self):
+        """
+        Returns the integer value of the current token
+        :return:
+        """
         return
 
     def stringVal(self):
+        """
+        Returns the string value of the current token, without the double
+        quotes.
+        :return:
+        """
         return
